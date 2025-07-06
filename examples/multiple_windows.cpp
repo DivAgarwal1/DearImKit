c9bc70bc6e5b71672fbc1665cda9c1ec1a08b415
DivAgarwal1 <divca06@gmail.com>
2025-07-06 12:58:56 -0700

    Updated examples

--- examples/multiple_windows.cpp
+++ examples/multiple_windows.cpp
@@ -6,13 +6,13 @@
 
 class HelloWorldAgain : public DearImKit::Panel {
 public:
-    HelloWorldAgain(int num_times = 1) : DearImKit::Panel("Hello World Again"), m_num_times(num_times) {};
+    HelloWorldAgain(int index, int num_times = 1) : DearImKit::Panel("Hello World Again"), m_index(index), m_num_times(num_times) {};
 
     bool draw() override {
         bool stay_open = true;
 
         // Check out https://github.com/ocornut/imgui for guide to ImGui logic
-        if (!ImGui::Begin(DearImKit::Panel::getName().c_str(), &stay_open)) {
+        if (!ImGui::Begin((DearImKit::Panel::getName() + "##" + std::to_string(m_index)).c_str(), &stay_open)) {
             ImGui::End();
             return stay_open;
         }
@@ -27,6 +27,7 @@ public:
 
 private:
     int m_num_times;
+    int m_index;
 };
 
 class HelloWorld : public DearImKit::Panel {
@@ -48,7 +49,8 @@ public:
 
         if (ImGui::Button("Say Hello World again")) {
             // HelloWorld2 will now be rendered
-            DearImKit::AddPanel<HelloWorldAgain>(3);
+            DearImKit::AddPanel<HelloWorldAgain>(m_num_windows_opened, 3);
+            m_num_windows_opened++;
         }
 
         ImGui::End();
@@ -57,6 +59,8 @@ public:
 
 private:
     int m_num_times;
+
+    int m_num_windows_opened = 0;
 };
 
 int main() {
