646ee67c016ebc24ef7026436fd8bc01e8f7c8db
DivAgarwal1 <divca06@gmail.com>
2026-03-09 22:36:30 -0500

    Move c abi away

--- c/src/init/init.cpp
+++
@@ -1,8 +0,0 @@
-#include "../../include/DearImKit/init/init.h"
-#include <tuple>
-
-#include "DearImKit/init/init.h"
-
-int DearImKit_SetupApp(const char *app_name, void (*at_start)(), void (*during_loop)(), DearImKit_BackgroundColor background_color) {
-    return DearImKit::SetupApp(app_name, at_start, during_loop, std::make_tuple(background_color.r, background_color.g, background_color.b, background_color.a));
-}
